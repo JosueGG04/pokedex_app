30d580db3ff425585f7a13efb7d929c7e4cd1c5e
Josue Garcia <jdgg0001@ce.pucmm.edu.do>
2024-11-08 22:44:36 -0400

    feat: type colors map

---
+++ lib/core/utils/type_colors.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+
+final Map<String, Color> typeColors = {
+  'bug': const Color.fromARGB(255, 159, 159, 40),
+  'dark': const Color.fromARGB(255, 79, 71, 71),
+  'dragon': const Color.fromARGB(255, 87, 111, 188),
+  'electric': const Color.fromARGB(255, 223, 188, 40),
+  'fairy': const Color.fromARGB(255, 226, 145, 226),
+  'fighting': const Color.fromARGB(255, 228, 144, 33),
+  'fire': const Color.fromARGB(255, 228, 97, 62),
+  'flying': const Color.fromARGB(255, 116, 170, 208),
+  'ghost': const Color.fromARGB(255, 111, 69, 112),
+  'grass': const Color.fromARGB(255, 67, 152, 55),
+  'ground': const Color.fromARGB(255, 164, 115, 60),
+  'ice': const Color.fromARGB(255, 100, 207, 207),
+  'poison': const Color.fromARGB(255, 130, 130, 130),
+  'normal': const Color.fromARGB(255, 147, 84, 203),
+  'psychic': const Color.fromARGB(255, 233, 108, 141),
+  'rock': const Color.fromARGB(255, 169, 164, 129),
+  'steel': const Color.fromARGB(255, 116, 176, 203),
+  'water': const Color.fromARGB(255, 48, 153, 225)
+};
