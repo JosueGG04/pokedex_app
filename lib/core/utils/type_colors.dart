ef06aea9b7354e3d6428f4d5a9ea2ee6a80790d5
Josue Garcia <jdgg0001@ce.pucmm.edu.do>
2024-11-11 00:32:46 -0400

    feat: new light type colors map

--- lib/core/utils/type_colors.dart
+++ lib/core/utils/type_colors.dart
@@ -13,10 +13,31 @@ final Map<String, Color> typeColors = {
   'grass': const Color.fromARGB(255, 67, 152, 55),
   'ground': const Color.fromARGB(255, 164, 115, 60),
   'ice': const Color.fromARGB(255, 100, 207, 207),
-  'poison': const Color.fromARGB(255, 130, 130, 130),
-  'normal': const Color.fromARGB(255, 147, 84, 203),
+  'normal': const Color.fromARGB(255, 130, 130, 130),
+  'poison': const Color.fromARGB(255, 147, 84, 203),
   'psychic': const Color.fromARGB(255, 233, 108, 141),
   'rock': const Color.fromARGB(255, 169, 164, 129),
   'steel': const Color.fromARGB(255, 116, 176, 203),
   'water': const Color.fromARGB(255, 48, 153, 225)
+};
+
+final Map<String, Color> lightTypeColors = {
+  'bug': const Color.fromARGB(255, 199, 199, 80),
+  'dark': const Color.fromARGB(255, 119, 111, 111),
+  'dragon': const Color.fromARGB(255, 127, 151, 228),
+  'electric': const Color.fromARGB(255, 253, 218, 70),
+  'fairy': const Color.fromARGB(255, 246, 185, 246),
+  'fighting': const Color.fromARGB(255, 248, 184, 73),
+  'fire': const Color.fromARGB(255, 248, 137, 102),
+  'flying': const Color.fromARGB(255, 156, 210, 248),
+  'ghost': const Color.fromARGB(255, 151, 109, 152),
+  'grass': const Color.fromARGB(255, 107, 192, 95),
+  'ground': const Color.fromARGB(255, 204, 155, 100),
+  'ice': const Color.fromARGB(255, 140, 247, 247),
+  'normal': const Color.fromARGB(255, 170, 170, 170),
+  'poison': const Color.fromARGB(255, 187, 124, 243),
+  'psychic': const Color.fromARGB(255, 253, 148, 181),
+  'rock': const Color.fromARGB(255, 209, 204, 169),
+  'steel': const Color.fromARGB(255, 156, 216, 243),
+  'water': const Color.fromARGB(255, 88, 193, 255)
 };
