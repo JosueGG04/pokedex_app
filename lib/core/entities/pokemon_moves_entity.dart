06ece40830644a7a1ff2569bcf6129b711e700c7
Josue Garcia <jdgg0001@ce.pucmm.edu.do>
2024-12-02 16:52:00 -0400

    feat: Enhance PokemonMovesEntity with additional properties and grouping methods

--- lib/core/entities/pokemon_moves_entity.dart
+++ lib/core/entities/pokemon_moves_entity.dart
@@ -5,7 +5,10 @@ class PokemonMovesEntity {
   int pp;
   int? power;
   int level;
-  List<dynamic> type;
+  String type;
+  String damageClass;
+  String learningMethod;
+  int learningMethodId;
 
   PokemonMovesEntity({
     required this.name,
@@ -14,5 +17,37 @@ class PokemonMovesEntity {
     required this.power,
     required this.level,
     required this.type,
+    required this.damageClass,
+    required this.learningMethod,
+    required this.learningMethodId,
   });
+
+  static learningMethodCategory(int id) {
+    if (id == 1) {
+      return 'Level up';
+    } else if (id == 4) {
+      return 'Machine';
+    } else{
+      return 'Others';
+    }
+  }
+
+  static Map<String, List<PokemonMovesEntity>> groupByMethod(
+      List<PokemonMovesEntity> moves) {
+    Map<String, List<PokemonMovesEntity>> groupedMoves = {
+      'Level up': [],
+      'Machine': [],
+      'Others': [],
+    };
+
+    for (var move in moves) {
+      groupedMoves[learningMethodCategory(move.learningMethodId)]!.add(move);
+    }
+
+    return groupedMoves;
+  }
+
+  static int compareByLevel(PokemonMovesEntity a, PokemonMovesEntity b) {
+    return a.level.compareTo(b.level);
+  }
 }
